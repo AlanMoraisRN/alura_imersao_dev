3e147b2e8f7089ac583a06eca779095716c33e3a
Alan_Morais <alanmoraisrn@gmail.com>
2022-03-10 20:21:41 -0300

    AULA 03 IMERSÃO DEV - ALURA

---
+++ PROJETOS/MENTALISTA/mentalistaid3/mentalistaid3/src/script.js
@@ -0,0 +1,18 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+
+function Chutar() {
+  let chute = parseInt(document.getElementById("valor").value);
+  let elementoResultado = document.getElementById("resultado");
+  console.log(chute);
+
+  if (chute == numeroSecreto) {
+    elementoResultado.innerHTML = "PARABÉNS! VOCÊ ACERTOU O NÚMERO!";
+    console.log("O NÚMERO ESTÁ CORRETO");
+  } else if (chute < 0 || chute > 10) {
+    elementoResultado.innerHTML = "VOCÊ DEVE DIGITAR UM NÚMERO DE 0 A 10";
+    console.log("VOCÊ DEVE DIGITAR UM NÚMERO DE 0 A 10");
+  } else {
+    elementoResultado.innerHTML = "NÚMERO ERRADO, TENTE NOVAMENTE!";
+    console.log("O NÚMERO ESTÁ ERRADO!");
+  }
+}
